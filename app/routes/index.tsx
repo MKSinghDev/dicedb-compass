1899b78062aafcea5711b08c8f98c6a37da012e1
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-03-28 16:24:47 +0530

    fix: title updated

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Welcome } from '~/client/welcome/welcome';
 
 export function meta() {
-    return [{ title: 'Typesense UI | Developed by MKSingh' }, { name: 'description', content: 'Typesense UI/Console | Developed by MKSingh' }];
+    return [{ title: 'DiceDB Insight | Developed by MKSingh' }, { name: 'description', content: 'DiceDB Insight | Developed by MKSingh' }];
 }
 
 export default function Home() {
